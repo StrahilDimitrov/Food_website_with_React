8f2f535a8210cdc8297e9dc0e9947e96d77dca78
Strahil Hristov <strahil1998@abv.bg>
2023-11-16 15:55:37 +0200

    components

---
+++ food/src/helpers/MenuList.js
@@ -0,0 +1,33 @@
+import Salad from '../assets/salad.jpg';
+import Pizza from '../assets/pizza.jpg';
+import Sushi from '../assets/sushi.jpg';
+import Drings from '../assets/drings.jpg';
+import Hamburger from '../assets/hamburger.jpg';
+import Dessert from '../assets/desserts.jpg';
+
+export const MenuList = [
+    {
+        name: "Salad",
+        image: Salad
+    },
+    {
+        name: "Pizza",
+        image: Pizza
+    },
+    {
+        name: "Sushi",
+        image: Sushi
+    },
+    {
+        name: "Drings",
+        image: Drings
+    },
+    {
+        name: "Hamburger",
+        image: Hamburger
+    },
+    {
+        name: "Dessert",
+        image: Dessert
+    }
+]
