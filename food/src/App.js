7a632a158fb506fa04526945132a100c14924d85
Strahil Hristov <strahil1998@abv.bg>
2023-11-16 15:57:17 +0200

    update App

--- food/src/App.js
+++ food/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/menu' element={<Menu />} />
         </Routes>
         <Footer />
       </BrowserRouter>
